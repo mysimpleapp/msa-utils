93e80345dc2d7d8e3c1eda0ffce5464dad727274
Nicolas CARREZ <ncarrez@csod.com>
2020-05-09 15:41:52 +0200

    format code

--- htmlExpr.js
+++ htmlExpr.js
@@ -5,15 +5,15 @@ const { join, resolve, basename } = require('path')
 const htmlparser2 = require("htmlparser2")
 
 // convert HTML expression to HTML object
-exp.formatHtml = function(htmlExpr, res) {
-	if(!res) res = {}
+exp.formatHtml = function (htmlExpr, res) {
+	if (!res) res = {}
 	// fill head & body objects
 	var head = new Set(), body = []
 	_formatHtml_core(htmlExpr, head, body, false)
 	// format head & body to sring
 	var bodyStr = body.join('')
 	var headStr = ""
-	for(var h of head)
+	for (var h of head)
 		headStr += h
 	// return HTML string
 	res.head = headStr
@@ -23,15 +23,15 @@ exp.formatHtml = function(htmlExpr, res) {
 function _formatHtml_core(htmlExpr, head, body, isHead) {
 	var type = typeof htmlExpr
 	// case string
-	if(type==="string") {
+	if (type === "string") {
 		_formatHtml_push(htmlExpr, head, body, isHead)
-	} else if(type==="object") {
+	} else if (type === "object") {
 		// case array
 		var len = htmlExpr.length
-		if(len!==undefined) {
-			for(var i=0; i<len; ++i)
+		if (len !== undefined) {
+			for (var i = 0; i < len; ++i)
 				_formatHtml_core(htmlExpr[i], head, body, isHead)
-		// case object
+			// case object
 		} else {
 			var tag = htmlExpr.tag
 			var cnt = htmlExpr.content || htmlExpr.cnt
@@ -43,19 +43,19 @@ function _formatHtml_core(htmlExpr, head, body, isHead) {
 			var css = htmlExpr.stylesheet || htmlExpr.css
 			var wel = htmlExpr.webelement || htmlExpr.wel
 			// web element
-			if(wel) {
+			if (wel) {
 				const ext = wel.split(".").pop()
-				if(ext === "html"){
-					_formatHtml_core({ import:wel }, head, body, isHead)
+				if (ext === "html") {
+					_formatHtml_core({ import: wel }, head, body, isHead)
 					tag = tag || basename(wel, '.html')
-				} else if(ext === "js"){
-					_formatHtml_core({ mod:wel }, head, body, isHead)
+				} else if (ext === "js") {
+					_formatHtml_core({ mod: wel }, head, body, isHead)
 					tag = tag || basename(wel, '.js')
 				}
 				isHead = false
 			}
 			// js module
-			if(mod && !tag) {
+			if (mod && !tag) {
 				tag = 'script'
 				attrs = attrs || {}
 				attrs.src = mod
@@ -63,7 +63,7 @@ function _formatHtml_core(htmlExpr, head, body, isHead) {
 				isHead = true
 			}
 			// html import
-			if(imp && !tag) {
+			if (imp && !tag) {
 				tag = 'link'
 				attrs = attrs || {}
 				attrs.rel = 'import'
@@ -71,14 +71,14 @@ function _formatHtml_core(htmlExpr, head, body, isHead) {
 				isHead = true
 			}
 			// script
-			if(js && !tag) {
+			if (js && !tag) {
 				tag = 'script'
 				attrs = attrs || {}
 				attrs.src = js
 				isHead = true
 			}
 			// stylesheet
-			if(css && !tag) {
+			if (css && !tag) {
 				tag = 'link'
 				attrs = attrs || {}
 				attrs.rel = 'stylesheet'
@@ -88,27 +88,27 @@ function _formatHtml_core(htmlExpr, head, body, isHead) {
 			}
 			// tag (with attrs, style & content)
 			tag = htmlExpr.tag || tag
-			if(tag) {
-				var str = '<'+tag
+			if (tag) {
+				var str = '<' + tag
 				// attrs
-				if(style) {
-					if(!attrs) attrs={}
+				if (style) {
+					if (!attrs) attrs = {}
 					attrs.style = style
 				}
-				if(attrs)
-					for(var a in attrs) {
+				if (attrs)
+					for (var a in attrs) {
 						var val = attrs[a]
-						if(val !== undefined) {
-							if(style) { a='style'; val=style }
-							if(a=='style') val = _formatHtml_style(val)
-							str += ' '+ a +'="'+ val +'"'
+						if (val !== undefined) {
+							if (style) { a = 'style'; val = style }
+							if (a == 'style') val = _formatHtml_style(val)
+							str += ' ' + a + '="' + val + '"'
 						}
 					}
 				str += '>'
 				_formatHtml_push(str, head, body, isHead)
 				// content
-				if(cnt) _formatHtml_core(cnt, head, body, isHead)
-				_formatHtml_push('</'+tag+'>', head, body, isHead)
+				if (cnt) _formatHtml_core(cnt, head, body, isHead)
+				_formatHtml_push('</' + tag + '>', head, body, isHead)
 			}
 			// body
 			_formatHtml_core(htmlExpr.body, head, body, false)
@@ -119,28 +119,28 @@ function _formatHtml_core(htmlExpr, head, body, isHead) {
 }
 function _formatHtml_style(style) {
 	var type = typeof style
-	if(type==="string") return style
-	else if(type==="object") {
+	if (type === "string") return style
+	else if (type === "object") {
 		var str = ""
-		for(var a in style) str += a+':'+style[a]+'; '
+		for (var a in style) str += a + ':' + style[a] + '; '
 		return str
 	}
 }
 function _formatHtml_push(html, head, body, isHead) {
-	if(isHead) head.add(html)
+	if (isHead) head.add(html)
 	else body.push(html)
 }
 
 // Check that an object match an user expr
 function checkHtmlExpr(match, htmlExpr) {
 	var tag = match.tag
-	if(tag && tag!==htmlExpr.tag) return false
+	if (tag && tag !== htmlExpr.tag) return false
 	var attrs = match.attrs
-	if(attrs) {
+	if (attrs) {
 		var htmlAttrs = htmlExpr.attrs
-		if(!htmlAttrs) return false
-		for(var p in attrs) {
-			if(attrs[p]!==htmlAttrs[p]) return false
+		if (!htmlAttrs) return false
+		for (var p in attrs) {
+			if (attrs[p] !== htmlAttrs[p]) return false
 		}
 	}
 	return true
@@ -149,25 +149,25 @@ exp.checkHtmlExpr = checkHtmlExpr
 
 // html parser ///////////////////////////////////////////////////////////////////
 
-exp.parseHtml = function(html) {
-	var domStack = [{tag:"root", content:[]}]
+exp.parseHtml = function (html) {
+	var domStack = [{ tag: "root", content: [] }]
 	var parser = new htmlparser2.Parser({
-		onopentag: function(tag, attrs){
+		onopentag: function (tag, attrs) {
 			var newDom = {
 				tag: tag,
 				attrs: attrs,
 				content: []
 			}
-			domStack[domStack.length-1].content.push(newDom)
+			domStack[domStack.length - 1].content.push(newDom)
 			domStack.push(newDom)
 		},
-		ontext: function(text){
-		    domStack[domStack.length-1].content.push(text)
+		ontext: function (text) {
+			domStack[domStack.length - 1].content.push(text)
 		},
-		onclosetag: function(tag){
+		onclosetag: function (tag) {
 			domStack.pop()
 		}
-	}, {decodeEntities: true})
+	}, { decodeEntities: true })
 	parser.write(html)
 	parser.end()
 	return { body: domStack[0].content }
